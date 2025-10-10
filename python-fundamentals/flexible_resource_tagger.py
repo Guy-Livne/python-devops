8e986e3a0cf5bee0bad7eef49ae7a9bb655b5926
Guy Livne'git config --global user.email guylivne123@gmail.comgit config --global user.name "Guy Livne <guylivne123@gmail.com>
2025-10-10 16:17:03 +0300

    modified the answer because later in the course there was an extension fo the exercise called Validating Inputs for Flexible Resource Tagger in the error handling section

--- python-fundamentals/flexible_resource_tagger.py
+++ python-fundamentals/flexible_resource_tagger.py
@@ -1,2 +1,36 @@
-dicts= {"key1": "value1", "key2": "value2"}
-print(dicts.keys())
+def manage_tags(existing_tags: dict, *simple_tags, **key_value_tags) -> dict:
+    """
+    Applies simple and key-value tags to an existing dictionary of tags.
+
+    Args:
+        existing_tags: The initial dictionary of tags.
+        *simple_tags: Positional string arguments to be added as tags with a
+                      value of 'true'. Duplicates are ignored.
+        **key_value_tags: Keyword arguments to be added or used to overwrite
+                          existing tags.
+
+    Returns:
+        A new dictionary with all tags merged.
+
+    Raises:
+        TypeError: If existing_tags is not a dictionary.
+    """
+    # TODO: Add validation here.
+    # Check if 'existing_tags' is a dictionary.
+    # If it's not, raise a TypeError with a helpful message.
+
+    if not isinstance(existing_tags,dict):
+        raise TypeError("must be a dictionary")
+        
+    ans=existing_tags.copy()
+    key_new_tags= dict(key_value_tags)
+    new_tags= list(set(simple_tags) - set(existing_tags.keys()))
+    
+    for tag in new_tags:
+        ans[tag]='true'
+    
+    for key in key_new_tags:
+        ans[key]= key_new_tags[key]
+        
+    
+    return ans
